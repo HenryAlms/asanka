bfd5204234805881797b51ba9cef5893c376746a
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2018-02-23 13:34:36 -0800

    continue setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+production: true,
+firebase: {
+apiKey: "AIzaSyBpnwl8afPChZ8Pha_hU1KfaeYbenQrrVA",
+authDomain: "asanka-aca.firebaseapp.com",
+databaseURL: "https://asanka-aca.firebaseio.com",
+projectId: "asanka-aca",
+storageBucket: "asanka-aca.appspot.com",
+messagingSenderId: "133622708380"
+}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+production: false,
+firebase: {
+apiKey: "AIzaSyBpnwl8afPChZ8Pha_hU1KfaeYbenQrrVA",
+authDomain: "asanka-aca.firebaseapp.com",
+databaseURL: "https://asanka-aca.firebaseio.com",
+projectId: "asanka-aca",
+storageBucket: "asanka-aca.appspot.com",
+messagingSenderId: "133622708380"
+}
+};
