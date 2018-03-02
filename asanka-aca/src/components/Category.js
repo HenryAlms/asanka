fecbbece9532122e75e6903b338c8cd25d89ab35
Benneth Cenita <bennethcenita@D-140-142-143-115.dhcp4.washington.edu>
2018-03-02 13:08:12 -0800

    stash

--- asanka-aca/src/components/Category.js
+++ asanka-aca/src/components/Category.js
@@ -1,16 +1,15 @@
 import React from 'react';
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
+// import Content from './Content'
 
 export default class Category extends React.Component {
 
     render() {
         let catName = this.props.name;
-        console.log(catName);
         return (
             <li>
-               {catName}
+                <a href="#">
+                    {catName}
+                </a>
             </li>
         );
     }
