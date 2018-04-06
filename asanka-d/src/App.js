984155e2c88a0f5311059f95be3121f39422e8a8
Henry Alms <henryalms17@gmail.com>
2018-04-05 18:26:51 -0700

    addded app & firebase stuff

--- asanka-d/src/App.js
+++ asanka-d/src/App.js
@@ -1,20 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import constants from "./components/constants";
+import { Col, Row } from 'reactstrap';
+import Welcome from "./components/homepage";
+import Header from "./components/header";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import Content from "./components/Content"
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    this.unregisterFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      if (firebaseUser) {
+        this.setState({ user: firebaseUser, loading: false });
+      }
+      else {
+        this.setState({ user: null, loading: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unregisterFunction();
+  }
+
+  handleSignIn(email, password) {
+    this.setState({ errorMessage: null });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch((err) => this.setState({ errorMessage: err.message }))
+  }
+
+  handleSignOut() {
+    this.setState({ errorMessage: null });
+    firebase.auth().signOut()
+      .catch((err) => this.setState({ errorMessage: err.message }))
+  }
+
   render() {
+    // this is how you pass props to the components you render through the switch. pass props here, 
+    //and use "render={renderComponentName}" in the switch
+    // let renderWelcome = (routerProps) => {
+    //   return <Welcome {...routerProps} user={this.state.user} signInCallback={(email, password) => this.handleSignIn(email, password)}  />
+    // }
+    // let renderDashboard = (routerProps) => {
+    //   return <Dashboard {...routerProps} user={this.state.user} />
+    // }
+    // let renderProfile = (routerProps) => {
+    //   return <Profile {...routerProps} user={this.state.user} />
+    // }
+    // let renderUsers = (routerProps) => {
+    //   return <Users {...routerProps} user={this.state.user} />
+    // }
+    // let renderDevices = (routerProps) => {
+    //   return <Devices {...routerProps} user={this.state.user} />
+    // }  
+    // let renderContent = (routerProps) => {
+    //   return <Content {...routerProps} user={this.state.user}/>
+    // }
+    // console.log(this.state.user);      
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <BrowserRouter>
+        <Row>
+          {this.state.user && <MyNav signOutCallback={() => this.handleSignOut()}/>}
+          {this.state.user && <Header />}
+          <Col>
+            <div className="switch">  
+              <Switch>
+                <Route path={constants.routes.welcome} render={renderWelcome} />
+                {/* <Route exact path={constants.routes.dashboard} render={renderDashboard} />
+                <Route path={constants.routes.welcome} render={renderWelcome} />
+                <Route path={constants.routes.profile} render={renderProfile} />
+                <Route path={constants.routes.users} render={renderUsers} />
+                <Route path={constants.routes.devices} render={renderDevices} />
+                <Route path={constants.routes.content} render={renderContent} /> */}
+              </Switch>
+            </div>  
+          </Col>
+        </Row>  
+      </BrowserRouter>
+    )
   }
 }
 
---
+++ asanka-d/src/components/header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Col, Container, Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import '../css/Header.css';
+import constants from './constants';
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <Navbar className="fixed-top header" color="faded" light>
+                <NavbarBrand href="/" className="justify-content-center pt-3 pl-2"><p className="navBrand">Asanka</p></NavbarBrand>
+            </Navbar>    
+        )
+    }
+}
