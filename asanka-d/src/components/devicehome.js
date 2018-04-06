ce2b8a935abf687e50fe5bc305656b25386bc211
Henry Alms <henryalms17@gmail.com>
2018-04-06 14:27:29 -0700

    added device homepage to gh

--- asanka-d/src/components/devicehome.js
+++ asanka-d/src/components/devicehome.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import constants from './constants';
+import "../css/device.css"
 
 export default class DeviceHome extends React.Component {
     constructor(props) {
@@ -17,11 +18,16 @@ export default class DeviceHome extends React.Component {
     render() {
         return(
             <div>
-                <div>
+                <div class="row" id="welcome">
+                    <div className="message">
+                        <strong>Hello Henry Alms, </strong> What would you like to learn today?
+                    </div>
+                </div>
+                <div className="background">
                 <div class="row">
                     <div class="col-sm-3">
                         <div class="card">
-                        <img class="card-img-top" src="./imgs/AsankaBackground.png" alt="Card image cap"/>
+                        <img class="card-img-top" src="./imgs/AsankaBackground.jpg" alt="Card image cap"/>
                         <div class="card-body">
                             <h5 class="card-title">Personal Files</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -31,7 +37,7 @@ export default class DeviceHome extends React.Component {
                     </div>
                     <div class="col-sm-3">
                         <div class="card">
-                        <img class="card-img-top" src="./imgs/Asanka-logo.png" alt="Card image cap"/>
+                        <img class="card-img-top" src="./imgs/openEdRes.jpg" alt="Card image cap"/>
                         <div class="card-body">
                             <h5 class="card-title">Free Resources</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -41,7 +47,7 @@ export default class DeviceHome extends React.Component {
                     </div>
                     <div class="col-sm-3">
                         <div class="card">
-                        <img class="card-img-top" src="./imgs/AsankaBackground.png" alt="Card image cap"/>
+                        <img class="card-img-top" id="ges" src="./imgs/ges.png" alt="Card image cap"/>
                         <div class="card-body">
                             <h5 class="card-title">GES Content</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -51,11 +57,11 @@ export default class DeviceHome extends React.Component {
                     </div>
                     <div class="col-sm-3">
                         <div class="card">
-                            <img class="card-img-top" src="./imgs/Asanka-logo.png" alt="Card image cap"/>
+                            <img class="card-img-top" src="./imgs/Baaba Game.png" alt="Card image cap"/>
                         <div class="card-body">
                             <h5 class="card-title">Interactive Games</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-danger">Interactive Games</a>
+                            <Link to={constants.routes.aca}><a href="#" class="btn btn-danger">Interactive Games</a></Link>
                         </div>
                         </div>
                     </div>
